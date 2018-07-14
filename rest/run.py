5b9bd35cc238ae6aa171a6de2fdccc788589b7b4
Ryan Irey <31294549+Ryan-Irey@users.noreply.github.com>
2018-07-13 20:23:39 -0500

    Update run.py

--- rest/run.py
+++ rest/run.py
@@ -1,46 +1,7 @@
 # REST Demo
 from eve import Eve
-import platform
 
-settings = {
-    'MONGO_HOST':'localhost',
-    'MONGO_PORT':27017,
-    'MONGO_DBNAME':'testDB',
-    'DOMAIN':{'contacts':{}}
-}
-
-app = Eve(settings = settings)
-
-@app.route('/processor')
-def processor():
-    name = platform.processor()
-    return name
-
-@app.route('/ram')
-def processor():
-    ram = platform.ram()
-    return ram
-
-@app.route('/files')
-def processor():
-    files = platform.files()
-    return files
-
-@app.route('/diskspace')
-def processor():
-    diskspace = platform.diskspace()
-    return diskspace
-
-@app.route('/who')
-def processor():
-    who = platform.who()
-    return who
-
-@app.route('/uname')
-def processor():
-    uname = platform.uname()
-    return uname
-
+app = Eve()
 
 if __name__ == "__main__":
     app.run(debug = True)
